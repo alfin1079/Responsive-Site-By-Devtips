f4007cd8fc2f0e68c1b166c9a424b46082fe300f
Muhammad Alfin <muhammad.alfin1079@gmail.com>
2015-09-13 22:05:59 +0700

    Add animation on work section

--- _site/js/function.js
+++ _site/js/function.js
@@ -1,6 +1,8 @@
 $(function(){
 	smoothScrool(1000);
+	workBelt();
 })
+
 
 // SmoothScrool function is applied from the document ready function
 function smoothScrool(duration){
@@ -13,5 +15,17 @@ function smoothScrool(duration){
 				scrollTop: target.offset().top
 			}, duration);
 		}
+	});
+}
+
+function workBelt(){
+	$('.thumb-unit').click(function(){
+		$('.work-belt').css('left','-100%');
+		$('.work-container').show();
+	});
+
+	$('.work-return').click(function(){
+		$('.work-belt').css('left','0%');
+		$('.work-container').hide(800);
 	});
 }
