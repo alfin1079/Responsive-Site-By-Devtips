750bc3104e2a98f76a3ad3a3a12840d21ca0de89
Muhammad Alfin <muhammad.alfin1079@gmail.com>
2015-09-17 16:11:08 +0700

    Dynamic data on work section

--- js/function.js
+++ js/function.js
@@ -1,6 +1,7 @@
 $(function(){
 	smoothScrool(1000);
 	workbelt();
+	workLoad();
 })
 
 // SmoothScrool function is applied from the document ready function
@@ -26,6 +27,21 @@ function workbelt(){
 	$(".work-return").click(function(){
 		$(".work-belt").css('left','0%');
 		$('.work-container').hide(800); 
+	});
+}
+
+function workLoad(){
+	$.ajaxSetup({ cache: false });
+
+	$(".thumb-unit").click(function(){
+		var $this = $(this),
+			newTitle = $this.find('strong').text(),
+			newFolder = $this.data('folder'),
+			spinner = '<div class="loader">Loading...</div>',
+			folder = $(this).data(folder),
+			newHTML = '/work/'+newFolder+'.html';
+		$('.project-load').html(spinner).load(newHTML);
+		$('.project-title').text(newTitle);
 	});
 }
 
